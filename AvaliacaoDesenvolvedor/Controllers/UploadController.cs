18080e26190916b43fc9e33e6a934e9acca901a7
Paulo <paulo.1209@terra.com.br>
2017-03-28 00:48:38 -0300

    Primeiro comit

---
+++ AvaliacaoDesenvolvedor/Controllers/UploadController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+using System.Collections;
+using AvaliacaoDesenvolvedor.Models;
+
+namespace AvaliacaoDesenvolvedor.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UploadDeArquivo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadDeArquivo(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/uploads"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "Arquivo carregado com sucesso!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+        }
+        public ActionResult LerArquivos()
+        {
+
+            return View();
+        }
+
+       
+
+
+
+        
+    }
+}
---
+++ AvaliacaoDesenvolvedor/Models/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaliacaoDesenvolvedor.Models
+{
+    public class Pedido
+    {
+        public String Comprador { get; set; }
+        public String Descricao { get; set; }
+        public double PrecoUnitario { get; set; }
+        public int Quantidade { get; set; }
+        public String Endereco { get; set; }
+        public String Fornecedor { get; set; }
+    }
+}
